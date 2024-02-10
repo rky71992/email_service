67022a2964ed151ad32577f3ff1938a45df741a7
Rohit Yadav <rky71992@gmail.com>
2024-02-10 13:16:48 +0530

    user delete service added

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask
 
 from auth.authentication import auth_bp
-from mail.services import service_bp
 from mail.mail import mail_bp
 from user.user import user_bp
 from config import API_VERSION
 
+
 app = Flask(__name__)
 
-app.register_blueprint(user_bp,url_prefix=f'/{API_VERSION}')
-app.register_blueprint(service_bp,url_prefix=f'/{API_VERSION}')
-app.register_blueprint(mail_bp,url_prefix=f'/{API_VERSION}')
 app.register_blueprint(auth_bp,url_prefix=f'/{API_VERSION}')
+app.register_blueprint(user_bp,url_prefix=f'/{API_VERSION}')
+app.register_blueprint(mail_bp,url_prefix=f'/{API_VERSION}')
 
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
--- user/user.py
+++ user/user.py
@@ -1,11 +1,12 @@
 import logging
 from flask import Blueprint, request, jsonify
-from definations import NewUser
+from definations import NewUser, NewServiceRegister
 import utils
 from db import session
 from errors import MISSING_REQUIRED_PARAMETER, INVALID_PARAMETER
 from models import Users, Services, UserServices
 from auth.authentication import token_required
+from utils import my_abort
 
 logger = logging.getLogger("user.{}".format(__name__))
 user_bp = Blueprint('user_blueprint', __name__)
@@ -54,4 +55,65 @@ def get_user_details(user_id):
     data['services'] = services
     return jsonify(data)
 
+
+@user_bp.route('/user/add-service',methods=['POST'])
+@token_required
+def add_mail_service(user_id: int):
+    data: dict = request.get_json()
+    if not data or not data.get('services'):
+        return my_abort(MISSING_REQUIRED_PARAMETER)
+    
+    services: list[dict] = data['services']
+    validated_services: list[NewServiceRegister] = []
+    for service in services:
+        try:
+            service['user_id'] = user_id
+            new_service: NewServiceRegister = NewServiceRegister(**service)
+            validated_services.append(new_service)
+        except Exception as ex:
+            logger.exception(f'Not able to register new service: {ex}')
+            my_abort(INVALID_PARAMETER)
+
+    for service in validated_services:
+        session.add(UserServices(**service.model_dump()))
+    session.commit()
+    return jsonify({'success':True})
+    
+
+@user_bp.route('/user/update-service',methods=['POST'])
+@token_required
+def update_user_mail_service(user_id):
+    pass
+
+
+@user_bp.route('/user/delete-service',methods=['DELETE'])
+@token_required
+def delete_user_mail_service(user_id):
+    data: dict = request.get_json()
+    if 'services' not in data and isinstance(data.get('services'),list):
+        return my_abort(MISSING_REQUIRED_PARAMETER)
+    
+    service_delete_response : list = []
+    for service in data['services']:
+        service_resp = {'service_name':service}
+        service_resp['error'] = ''
+        master_service = session.query(Services).filter_by(service_name=service).one_or_none()
+        if not master_service:
+            service_resp['status'] = False
+            service_resp['error'] = "There is no service supported by this name"
+            service_delete_response.append(service_resp)
+            continue
+        user_registered_service = session.query(UserServices).filter_by(user_id=user_id,service_id=master_service.id).one_or_none()
+        if not user_registered_service:
+            service_resp['status'] = False
+            service_resp['error'] = "User have not registered this service"
+            service_delete_response.append(service_resp)
+            continue
+
+        session.delete(user_registered_service)
+        session.commit()
+        service_resp['status'] = True
+        service_delete_response.append(service_resp)
+    
+    return jsonify({"services":service_delete_response})
 
